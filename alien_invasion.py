56a526f5098ec08f63660ace2e3c5ea3aa4c5caf
dingding <dingliang@sf-express.com>
2018-05-18 14:37:21 +0800

    13.8

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,8 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from game_stats import GameStats
+
 
 
 def run_game():
@@ -16,17 +18,27 @@ def run_game():
     #screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption('Alien Invasion')
 
-    #创建一艘飞船
+    #创建一个用于存储游戏统计信息的实例
+    status = GameStats(ai_settings)
+
+    #创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings,screen)
     #创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
+
+    #创建一个外星人群
+    gf.create_fleet(ai_settings,screen,ship,aliens)
 
     #开始游戏的主循环
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_scrren(ai_settings,screen,ship,bullets)
+        if status.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,status,screen,ship,aliens,bullets)
+
+        gf.update_scrren(ai_settings,screen,ship,aliens,bullets)
 
         #让最近绘制的屏幕可见
         pygame.display.update()
--- settings.py
+++ settings.py
@@ -9,14 +9,23 @@ class Settings(object):
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
-        #飞船的移动速度
+        #飞船设置
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
         #子弹设置
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_speed_factor = 30
+        self.bullet_width = 900
+        #可以改变子弹的宽度,speed加快测试的进度
         self.bullet_height = 15
         self.bullet_color = 60,60,60
         self.bullets_allowed = 5
+
+        #外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 100
+        #fleet_direction为1表示向右移，为-1标示向左移
+        self.fleet_direction = 1
+
 
 
 
