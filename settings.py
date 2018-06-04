a366326cac234c57e03830b2c149ba29a799135c
dingding <dingliang@sf-express.com>
2018-06-04 21:01:55 +0800

    scoreboard

--- settings.py
+++ settings.py
@@ -10,21 +10,40 @@ class Settings(object):
         self.screen_height = 800
         self.bg_color = (230,230,230)
         #飞船设置
-        self.ship_speed_factor = 1.5
         self.ship_limit = 3
         #子弹设置
-        self.bullet_speed_factor = 30
         self.bullet_width = 900
         #可以改变子弹的宽度,speed加快测试的进度
         self.bullet_height = 15
         self.bullet_color = 60,60,60
-        self.bullets_allowed = 5
+        self.bullets_allowed = 3
 
         #外星人设置
+        self.fleet_drop_speed = 10
+
+        #以什么样的速度加快游戏节奏
+        self.speedup_scale = 2.0
+        self.initialize_dynamic_settings()
+
+
+    def initialize_dynamic_settings(self):
+        '''初始化随游戏进行而变化的设置'''
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
         self.alien_speed_factor = 1
-        self.fleet_drop_speed = 100
+
         #fleet_direction为1表示向右移，为-1标示向左移
         self.fleet_direction = 1
+
+        #计分
+        self.alien_points = 50
+
+
+    def increase_speed(self):
+        '''提高速度设置'''
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
 
 
 
