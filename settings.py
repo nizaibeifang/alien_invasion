cd4de4a1312c9375088ee8c30b8fca0380a4d08e
dingding <13823577321@163.com>
2018-06-05 23:31:15 +0800

    settings

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Settings(object):
         '''初始化游戏的设置'''
         #屏幕设置
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 700
         self.bg_color = (230,230,230)
         #飞船设置
         self.ship_limit = 3
